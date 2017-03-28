df4b3052db5bbd6b2da801e1b16e579c373a3fe2
jonghoon <jhwgnu@hanmail.net>
2017-03-28 17:55:51 +0900

    테스트 코드 추가 및 debts페이지 빼고 모두 read-only 페이지로 설정

--- debtpages_rest/views.py
+++ debtpages_rest/views.py
@@ -3,34 +3,32 @@ from .serializers import DebtSerializer, UserSerializer, UserSumSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
 
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
 
 class DebtList(generics.ListCreateAPIView):
     queryset = Debt.objects.all()
     serializer_class = DebtSerializer
 
 
-class DebtDetail(generics.RetrieveUpdateDestroyAPIView):
+class DebtDetail(generics.ListAPIView):
     queryset = Debt.objects.all()
     serializer_class = DebtSerializer
 
 
-class UserList(generics.ListCreateAPIView):
+class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserDetail(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserSumList(generics.ListCreateAPIView):
+class UserSumList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSumSerializer
 
 
-class UserSumDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserSumDetail(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSumSerializer
